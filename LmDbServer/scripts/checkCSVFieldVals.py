210792b2b898cb6ab62694aa1b986a7c00ab0ee2
zzeppozz <aimee.stewart@ku.edu>
2015-09-24 13:32:44 -0500

    Fixed check function

--- LmDbServer/scripts/checkCSVFieldVals.py
+++ LmDbServer/scripts/checkCSVFieldVals.py
@@ -1,20 +1,23 @@
 run = 3
-fname = 'sorted_gbif_borneo_simple_%d.csv' % run
+fname = 'smallsort_gbif_borneo_simple_%d.csv' % run
 
 def checkfile(fname):
    good26 = 0
    good27 = 0
    total = 0
+   ranks = set()
    f = open(fname, 'r')
    rdr = csv.reader(f, delimiter='\t')
    for row in rdr:
       total += 1
+      ranks.add(row[11])
       try:
          int(row[26])
       except:
          pass
       else:
          good26 += 1
+         
       try:
          int(row[27])
       except:
@@ -22,5 +25,6 @@ def checkfile(fname):
       else:
          good27 += 1
    f.close()
+   print 'Ranks {}'.format(ranks)
    print 'Total = %d, Good26 = %d, Good27 = %d' % (total, good26, good27)
       
