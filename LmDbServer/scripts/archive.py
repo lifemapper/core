c7e4902cf0d7a91969f12e535cd6e645351e3272
Aimee Stewart <zzeppozz@gmail.com>
2015-11-13 17:40:10 -0600

    =fine tune script to export only needed info

--- LmDbServer/scripts/archive.py
+++ LmDbServer/scripts/archive.py
@@ -24,16 +24,18 @@
           02110-1301, USA.
 """
 import argparse
+import csv
+import glob
 import mx.DateTime
 import os
 import subprocess
-import sys
 import tarfile
 
 from LmCommon.common.lmconstants import DEFAULT_POST_USER, OutputFormat
 from LmCommon.common.localconstants import ARCHIVE_USER
 from LmServer.common.datalocator import EarlJr
 from LmServer.common.lmconstants import MAL_STORE, LM_SCHEMA
+from LmServer.common.localconstants import APP_PATH
 from LmDbServer.populate.GBIF.sortGbifExport import datapath
  
 DEBUG = False
@@ -92,7 +94,17 @@ USER_DEPENDENCIES = [
 # ...............................................
 class SetPass:
    def __call__(self):
-      os.environ['PGPASSWORD'] = 'jetsons'
+      hostname = subprocess.check_output('hostname').strip()
+      if hostname == 'hera':
+         os.environ['PGPASSWORD'] = 'jetsons'
+      else:
+         # more /opt/lifemapper/rocks/etc/users | grep admin | awk '{print $2}'
+         fname = os.path.join(APP_PATH, 'rocks/etc/users')
+         cmd = 'cat {} | grep admin | awk \'!print $2?\''.format(fname)
+         cmd = cmd.replace('!', '{')
+         cmd = cmd.replace('?', '}')
+         pswd = subprocess.check_output(cmd).strip()
+         os.environ['PGPASSWORD'] = pswd
 
 # ...............................................
 def getTimestamp(dateonly=False):
@@ -116,7 +128,7 @@ def getFilename(outpath, basefname, dumptype, table=None, user=None):
    @param user: username string
    '''
    outfname = None
-   choices = ('db_schema', 'db_data', 'file_data', 'readme', 'table', 'log')
+   choices = ('db_schema', 'db_data', 'file_data', 'readme', 'table', 'table_restore', 'log')
    if dumptype not in choices:
       raise Exception('Unknown dumptype {}; choices are {}'.format(dumptype, choices))
    
@@ -135,6 +147,8 @@ def getFilename(outpath, basefname, dumptype, table=None, user=None):
       outfname = os.path.join(outpath, '{}.README'.format(basefname))
    elif dumptype == 'table':
       outfname = os.path.join(outpath, '{}{}'.format(basefname, OutputFormat.TXT))
+   elif dumptype == 'table_restore':
+      outfname = os.path.join(outpath, '{}{}'.format(basefname, OutputFormat.CSV))
    elif dumptype == 'log':
       outfname = os.path.join(outpath, '{}{}'.format(basefname, OutputFormat.LOG))
 
@@ -142,7 +156,7 @@ def getFilename(outpath, basefname, dumptype, table=None, user=None):
       
 # ...............................................
 def getColumns(dbcmd, fulltablename):
-   getColumnsStmt = ' {} --command=\"\d {}\" | grep \'|\' | awk \'\!print $1?\''.format(dbcmd, fulltablename)
+   getColumnsStmt = ' {} --command=\"\d {}\" | grep \'|\' | awk \'!print $1?\''.format(dbcmd, fulltablename)
    getColumnsStmt = getColumnsStmt.replace('!', '{')
    getColumnsStmt = getColumnsStmt.replace('?', '}')
    print 
@@ -155,34 +169,58 @@ def getColumns(dbcmd, fulltablename):
    return cols
 
 # ...............................................
-def copyDatabaseUsers(outpath, basefname, lmusers, dbcmd):
+def copyOutDatabaseUsers(outpath, basefname, lmusers, dbcmd, restrictToTables=None):
+   ingestfname = getFilename(outpath, basefname, 'table_restore')
    readmeLines = ['', '',
             'The following files contain the user-specific metadata contained ',
             'in the Lifemapper SDM.  Files are numbered according to the order ',
-            'in which they should be restored.' ]
+            'in which they should be restored.  Ingest statements are listed, ', 
+            'in order, in {} '.format(ingestfname) ]
    escapedUsers = ["\'{}\'".format(usr) for usr in lmusers]
    userSetStr = ', '.join(escapedUsers)
+   csvfile = open(ingestfname, 'wb')
+   csvwriter = csv.writer(csvfile, delimiter=CSV_DELIMITER)
    for i in range(len(USER_DEPENDENCIES)):
       (tablename, selstr) = USER_DEPENDENCIES[i]
-      cols = getColumns(dbcmd, tablename)
-      outfname = getFilename(outpath, basefname, 'table', table=(tablename, i))
-      queryStmt = selstr.replace(USER_REPLACE_STR, userSetStr)
-      copyToStmt = '\"COPY ({}) TO STDOUT \"'.format(queryStmt)
-      copyFromStmt = '\"COPY {} ({}) FROM STDIN \"'.format(tablename, ', '.join(cols))
-      dumptableStmt = '{} --command={}'.format(dbcmd, copyToStmt)
-      print 'Dumping table: ', tablename
-      with open(outfname, 'w') as outf:
-         if not DEBUG:
-            dProc = subprocess.Popen(dumptableStmt, stdout=outf, preexec_fn=SetPass, shell=True)
-      outf.close()
-      readmeLines.extend(['', 
-         '{}:'.format(outfname),
-         '    contains the metadata contained in the Lifemapper SDM',
-         '    database, table {}.  To append data existing database table, '.format(tablename),
-         '    execute: ',
-         '       {}'.format(copyFromStmt)])
+      if restrictToTables is None or tablename in restrictToTables:
+         cols = getColumns(dbcmd, tablename)
+         outfname = getFilename(outpath, basefname, 'table', table=(tablename, i))
+         queryStmt = selstr.replace(USER_REPLACE_STR, userSetStr)
+         copyToStmt = '\"COPY ({}) TO STDOUT \"'.format(queryStmt)      
+         dumptableStmt = '{} --command={}'.format(dbcmd, copyToStmt)
          
+         print 'Dumping table: ', tablename
+         with open(outfname, 'w') as outf:
+            dProc = subprocess.Popen(dumptableStmt, stdout=outf, preexec_fn=SetPass, shell=True)
+         outf.close()
+         
+         csvwriter.writerow([os.path.basename(outfname), tablename, ', '.join(cols)])
+         
+         readmeLines.extend(['', 
+            '{}:'.format(outfname),
+            '    contains the metadata contained in the Lifemapper SDM',
+            '    database, table {}, including the columns, in order. '.format(tablename)])
+   csvfile.close()
    return readmeLines
+      
+# ...............................................
+def ingestRecordData(inpath, inbasename, tablename, columnStr):
+   infname = os.path.join(inpath, inbasename)
+   copyFromStmt = '\"COPY {} ({}) FROM STDIN \"'.format(tablename, columnStr)
+   ingesttableStmt = '{} --command={}'.format(dbcmd, copyFromStmt)
+   print('Copying records from {} into {} ...'.format(ingestfname, tablename))
+   with open(infname, 'r') as inf:
+      if not DEBUG:
+         dProc = subprocess.Popen(ingesttableStmt, stdin=inf, preexec_fn=SetPass, shell=True)
+   inf.close()
+      
+# ...............................................
+def untarFileData(inpath, scriptname):
+   tarnames = glob.glob(os.path.join(inpath, scriptname+'*'+OutputFormat.TAR_GZ))
+   for tarball in tarnames:
+      cmd = 'tar -xvzf {}'.format(tarball)
+      dProc = subprocess.Popen(cmd, shell=True)
+      
       
 # ...............................................
 def dumpDbSchema(outpath, basefname, dbuser, dumpformat, dbschema, dbname):
@@ -236,9 +274,12 @@ def ignoreUser(usr, ignoreUsers, ignoreUserPrefixes):
    return ignoreMe
          
 # ...............................................
-def getActiveUsers(dbcmd, ignoreUsers, ignoreUserPrefixes, days=365):
-      yearago = mx.DateTime.gmt().mjd - 365
-      queryStmt = 'select distinct(userid) from occurrenceset where datelastmodified > {}'.format(yearago)
+def getActiveUsers(dbcmd, dbschema, defaultUser, ignoreUsers, ignoreUserPrefixes, days=180):
+      pastdate = mx.DateTime.gmt().mjd - days
+      queryStmt = ('select distinct(m.userid) from {0}.model m, {0}.scenario s '.format(dbschema)+
+                   'WHERE statusmodtime > {} '.format(pastdate)+
+                   'AND m.scenarioid = s.scenarioid '+
+                   'AND s.userid != \'{}\' '.format(defaultUser))
       cmd = '\"COPY ({}) TO STDOUT \"'.format(queryStmt)
       fullStmt = ' {} --command={}'.format(dbcmd, cmd)
       backupuserStr = subprocess.check_output(fullStmt, preexec_fn=SetPass, shell=True)
@@ -247,10 +288,12 @@ def getActiveUsers(dbcmd, ignoreUsers, ignoreUserPrefixes, days=365):
       for usr in backupusers:
          if not ignoreUser(usr, ignoreUsers, ignoreUserPrefixes):
             activeUsers.append(usr)
+      print 'Active Users: ', str(activeUsers)
       return activeUsers
 
 # ...............................................
-def getUsersToBackup(backupChoice, dbcmd, defaultUser, ignoreUsers=[], ignoreUserPrefixes=[]):
+def getUsersToBackup(backupChoice, dbcmd, dbschema, defaultUser, 
+                     ignoreUsers=[], ignoreUserPrefixes=[]):
    hostname = subprocess.check_output('hostname').strip()
    if hostname == 'hera':
       datapath = '/share/data/archive'
@@ -261,7 +304,7 @@ def getUsersToBackup(backupChoice, dbcmd, defaultUser, ignoreUsers=[], ignoreUse
       backupusers = [backupChoice]
    elif backupChoice == 'users':
       ignoreUsers.append(defaultUser)
-      backupusers = getActiveUsers(dbcmd, ignoreUsers=ignoreUsers, 
+      backupusers = getActiveUsers(dbcmd, dbschema, defaultUser, ignoreUsers=ignoreUsers, 
                                    ignoreUserPrefixes=ignoreUserPrefixes)
    elif backupChoice == 'all':
       backupusers = []
@@ -294,8 +337,9 @@ def dumpFileData(outpath, basefname, datapath, backupusers):
             '   To uncompress data, execute the following commands: '])
 
          outfname = getFilename(outpath, basefname, 'file_data', user=entry)
-         with tarfile.open(outfname, mode='w:gz') as archive:
-            archive.add(os.path.join(datapath, entry), recursive=True)
+         if not DEBUG:
+            with tarfile.open(outfname, mode='w:gz') as archive:
+               archive.add(os.path.join(datapath, entry), recursive=True)
          
          restoreCmd = 'tar -xvzf {}'.format(outfname)
          readmeLines.append('      {}'.format(restoreCmd)) 
@@ -318,75 +362,141 @@ def writeReadme(outpath, basefname, readmeLineLists):
 # ..............................................................................
 # MAIN
 # ..............................................................................
-if __name__ == '__main__':   
+# Note: Add any existing users to ignore list
+IGNORE_USERS = [DEFAULT_POST_USER, 'aimee', 'astewart', 'changeThinking', 'cgwillis', 
+               'pragma', 'unitTest', 'zeppo', 'aimee.stewart@ku.edu', 'cjgrady@ku.edu', 
+               'DermotYeti', 'DermotYeti2', 'DermotYeti3', 'DermotYeti4', 'camposds1', 
+               'kubi'] 
+IGNORE_USER_PREFIXES = ['CT', 'elseweb', 'Dermot', 'Workshop']
+ONLY_USERS = [ 'changeThinking'] 
+ONLY_USERS_PREFIXES = ['CT', 'elseweb']
+CSV_DELIMITER='\t'
+ARCHIVE_DEPENDENCIES = ['{}.lmuser'.format(LM_SCHEMA), 
+                        '{}.scenario'.format(LM_SCHEMA), 
+                        '{}.layertype'.format(LM_SCHEMA), 
+                        '{}.layer'.format(LM_SCHEMA), 
+                        '{}.scenariolayers'.format(LM_SCHEMA), 
+                        '{}.keyword'.format(LM_SCHEMA), 
+                        '{}.layertypekeyword'.format(LM_SCHEMA), 
+                        '{}.scenariokeywords'.format(LM_SCHEMA)]
+
+
+if __name__ == '__main__':
    parser = argparse.ArgumentParser(
-                description="Script to backup one or more user's data")
-   parser.add_argument('-o', "--outpath", type=str, 
-                        help='These are the users to reset jobs for')
-   group = parser.add_mutually_exclusive_group()
-   group.add_argument('-A', '--archive', help='Backup Archive data',
+                description="Script to backup or restore one or more users' data")
+   
+   parser.add_argument('command', type=str, choices=['backup','restore'], 
+                        help='Backup from existing database or restore to existing database')
+   
+   group1 = parser.add_mutually_exclusive_group()
+   group1.add_argument('-o', "--outpath", type=str, 
+                        help='Directory for output files')
+   group1.add_argument('-i', "--infile", type=str, 
+                        help='Input file containing lines of: '+
+                             'input record filename, tablename, column names; '+
+                             'input record files should be in the same directory')
+   
+   group2 = parser.add_mutually_exclusive_group()
+   group2.add_argument('-A', '--archive', help='Backup Archive data',
                        action='store_true')
-   group.add_argument('-U', '--users', help='Backup all (non-archive) user data (Default)',
+   group2.add_argument('-U', '--users', help='Backup all (non-archive) user data (Default)',
                        action='store_true')
-   group.add_argument('-a', '--all', help='Backup Archive and non-archive user data',
+   group2.add_argument('-a', '--all', help='Backup Archive and non-archive user data',
                        action='store_true')
-   group.add_argument('-u', '--singleUser', type=str, 
+   group2.add_argument('-u', '--singleUser', type=str, 
                        help='Backup a single user')
 
    args = parser.parse_args()
-   if args.users:
-      backupChoice = 'users'
-   elif args.all:
-      backupChoice = 'all'
-   elif args.archive:
-      backupChoice = 'kubi'
-   elif args.singleUser:
-      backupChoice = args.singleUser
+   cmd = args.command
+   
+   if cmd == 'backup':
+      if args.users:
+         backupChoice = 'users'
+      elif args.all:
+         backupChoice = 'all'
+      elif args.archive:
+         backupChoice = ARCHIVE_USER
+      elif args.singleUser:
+         backupChoice = args.singleUser
+      filepath = args.outpath
+      if not os.path.exists(filepath):
+         print '{} does not exist'.format(filepath)
+         exit(-1)
+         
+   elif cmd == 'restore':
+      ingestfname = args.infile
+      if not os.path.exists(ingestfname):
+         print '{} does not exist'.format(ingestfname)
+         exit(-1)
+      filepath = os.path.split(ingestfname)[0]
       
-   outpath = args.outpath
-   if not os.path.exists(outpath):
-      print '{} does not exist'.format(outpath)
-      exit(-1)
-
    # Naming variables
    scriptname = os.path.splitext(os.path.basename(__file__))[0]
    datestr = getTimestamp(dateonly=True).replace('-', '_')
-   basefname = '{}.{}.{}'.format(scriptname, backupChoice, datestr)
-   
-   logfname = getFilename(outpath, basefname, 'log')
+#    logfname = getFilename(filepath, basefname, 'log')
     
    # PostgreSQL dump/restore options
    dbuser = 'admin'
    dumpformat = 'custom'
    dbcmd = 'psql --username={} --dbname={} '.format(dbuser, MAL_STORE)
    
-   dbschemaHelp = [''] 
-   tableHelp = [''] 
-   tarballHelp = ['']
    
-   ignoreUsers = [DEFAULT_POST_USER, 'aimee', 'changeThinking', 'cgwillis', 
-                  'unitTest', 'zeppo'] 
-   ignoreUserPrefixes = ['CT', 'elseweb']
-   onlyUsers = [DEFAULT_POST_USER, 'aimee', 'changeThinking', 'cgwillis', 
-                  'unitTest', 'zeppo'] 
-   onlyUserPrefixes = ['CT', 'elseweb']
-   # Identify users for file backup
-   datapath, backupusers = getUsersToBackup(backupChoice, dbcmd, ARCHIVE_USER, 
-                                            ignoreUsers=ignoreUsers, 
-                                            ignoreUserPrefixes=ignoreUserPrefixes)
-   # Dump Schema and Database
-   dbschemaHelp = dumpDbSchema(outpath, basefname, dbuser, dumpformat, 
-                               LM_SCHEMA, MAL_STORE)
-   # Dump tables of user data 
-   tableHelp = copyDatabaseUsers(outpath, basefname, backupusers, dbcmd)
-   
-   # Backup, compress requested DATA_PATH/MODEL_PATH/<user> directories 
-   tarballHelp = dumpFileData(outpath, basefname, datapath, backupusers)
-   
-   # Explain outputs
-   readmeFname = writeReadme(outpath, basefname, 
-                             [dbschemaHelp, tableHelp, tarballHelp])
-   
-   print 'Instructions for using output from this script are in {}'.format(readmeFname)
-   
+   if cmd == 'backup':
+      basefname = '{}.{}.{}'.format(scriptname, backupChoice, datestr)
+      # Text for README
+      dbschemaHelp = [''] 
+      tableHelp = [''] 
+      tarballHelp = ['']
+      
+      # Identify users for file backup
+      cutoffDate = mx.DateTime.gmt().mjd - (180)
+      datapath, backupusers = getUsersToBackup(backupChoice, dbcmd, LM_SCHEMA, ARCHIVE_USER, 
+                                               ignoreUsers=IGNORE_USERS, 
+                                               ignoreUserPrefixes=IGNORE_USER_PREFIXES)
+      # Dump Schema and Database
+      dbschemaHelp = dumpDbSchema(filepath, basefname, dbuser, dumpformat, 
+                                  LM_SCHEMA, MAL_STORE)
+      # Dump tables of default input data 
+      if backupChoice == ARCHIVE_USER:
+         tableHelp = copyOutDatabaseUsers(filepath, basefname, backupusers, dbcmd, 
+                                          restrictToTables=ARCHIVE_DEPENDENCIES)
+      # Dump tables of user data 
+      else:
+         tableHelp = copyOutDatabaseUsers(filepath, basefname, backupusers, dbcmd)
+      
+#       # Backup, compress requested DATA_PATH/MODEL_PATH/<user> directories 
+#       if backupChoice != ARCHIVE_USER:
+#          tarballHelp = dumpFileData(filepath, basefname, datapath, backupusers)
+      
+      # Explain outputs
+      readmeFname = writeReadme(filepath, basefname, 
+                                [dbschemaHelp, tableHelp, tarballHelp])
+      
+      print 'Instructions for using output from this script are in {}'.format(readmeFname)
+      
+   elif cmd == 'restore':
+#       untarFileData(filepath, scriptname)
+      
+      with open(ingestfname) as csvfile:
+         reader = csv.reader(csvfile, delimiter=CSV_DELIMITER)
+         for row in reader:
+            baseinfname, tablename, columnStr = row
+            ingestRecordData(filepath, baseinfname, tablename, columnStr)
+      csvfile.close()
+      
+      '''
+      lm3.lmuser
+      lm3.lmjob
+      lm3.experiment
+      lm3.occurrenceset
+      lm3.scenario
+      lm3.layertype
+      lm3.layer
+      lm3.scenariolayers
+      lm3.keyword
+      lm3.layertypekeyword
+      lm3.scenariokeywords
+      lm3.model
+      lm3.projection
+      '''   
    
