c7c77505fe8de7fa0defa9ccca74b2d93f7f0fea
Aimee Stewart <zzeppozz@gmail.com>
2016-02-26 16:04:48 -0600

    removed old db changes

--- LmDbServer/dbsetup/updateDatabases.sql
+++ LmDbServer/dbsetup/updateDatabases.sql
@@ -1,23 +1,7 @@
 -- ----------------------------------------------------------------------------
 \c mal
 
-ALTER TABLE lm3.computeresource ADD COLUMN ipmask varchar(2);
-ALTER TABLE lm3.computeresource ADD CONSTRAINT computeresource_ipaddress_ipmask_key UNIQUE (ipaddress, ipmask);
-DROP FUNCTION lm3.lm_getCompute(ip varchar);
-DROP FUNCTION lm3.lm_insertCompute(cmpname varchar,
-                                                ip varchar,
-                                                domname text,
-                                                usr varchar,
-                                                createtime double precision);
 -- ----------------------------------------------------------------------------
 \c speco
-ALTER TABLE lm3.computeresource ADD COLUMN ipmask varchar(2);
-ALTER TABLE lm3.computeresource ADD CONSTRAINT computeresource_ipaddress_ipmask_key UNIQUE (ipaddress, ipmask);
-DROP FUNCTION lm3.lm_getCompute(ip varchar);
-DROP FUNCTION lm3.lm_insertCompute(cmpname varchar,
-                                                ip varchar,
-                                                domname text,
-                                                usr varchar,
-                                                createtime double precision);
 
 -- ----------------------------------------------------------------------------
