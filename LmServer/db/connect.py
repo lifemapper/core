c54985aceaffb0a16d6ca105f3296e93c9dfa733
Aimee Stewart <zzeppozz@gmail.com>
2016-04-12 11:08:00 -0500

    cleaned up gitignore

--- LmServer/db/connect.py
+++
@@ -1,8 +0,0 @@
-from LmServer.common.lmconstants import DbUser
-
-HL_NAME = {
-           DbUser.Pipeline: 'xfNWrz07oa8x',
-           DbUser.Map: '9jxGZ5rqpu2e',
-           DbUser.WebService: '3kc7zg7rLNli',
-           DbUser.Job: 'M4kJpeat8e5s',
-          }
