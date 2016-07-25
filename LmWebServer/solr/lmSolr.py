2aa072351a9f36042ab5e704eea084bb1354aa4d
CJ Grady <cjgrady@gmail.com>
2016-07-25 10:44:06 -0400

    Fix for case insensitive searches and added todo tag

--- LmWebServer/solr/lmSolr.py
+++ LmWebServer/solr/lmSolr.py
@@ -209,9 +209,11 @@ def searchHintIndex(name, retFormat, numColumns, maxReturned):
    rDict = literal_eval(resp)
    
    oHits = rDict['response']['docs']
+   
+   # TODO: This needs to be taken out once the Solr index is corrected
    hits = []
    for hit in oHits:
-      if hit['displayName'].startswith(name):
+      if hit['displayName'].lower().startswith(name).lower():
          hits.append(hit)
 
    if retFormat.lower() == "json":
