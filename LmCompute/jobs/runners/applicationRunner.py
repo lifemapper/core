c6cb4de28a00086d8b79fbc77a973ff1159266a8
CJ Grady <cjgrady@gmail.com>
2016-09-13 13:45:10 -0400

    First commit of application runner
    Greatly simplified now that we aren't sending back status / progress periodically.  May need more later.

--- LmCompute/jobs/runners/applicationRunner.py
+++ LmCompute/jobs/runners/applicationRunner.py
@@ -26,21 +26,19 @@
           Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
           02110-1301, USA.
 """
-import datetime
-from StringIO import StringIO
 from subprocess import Popen, PIPE
-import sys
 from time import sleep
 
 from LmCompute.jobs.runners.base import JobRunner
 
-from LmCommon.common.lmconstants import JobStatus
-from LmCompute.common.lmObj import LmException
 
 # Other
 # ............................
 WAIT_SECONDS = 30 # The number of seconds to wait, by default, between polling
+
+# TODO: Add mechanism to catch hung jobs
 HEARTBEAT_SECONDS = 4 * 60 * 60 # Four hours
+
 
 # .............................................................................
 class ApplicationRunner(JobRunner):
@@ -49,72 +47,16 @@ class ApplicationRunner(JobRunner):
                 to work on a job.
    """
    # ...................................
-   def __init__(self, job, env):
-      """
-      @summary: Constructor
-      @param jobType: The type of job to be run
-      @param jobId: The id of the job to run
-      @param env: The environment to run in
-      """
-      if self.__class__ == ApplicationRunner:
-         raise Exception("Abstract class ApplicationRunner should not be instantiated.")
-      JobRunner.__init__(self, job, env)
-      self.status = JobStatus.PULL_COMPLETE
-      self.progress = 0
-   
-   # ...................................
-   def run(self):
+   def _doWork(self):
       """
       @summary: Runs the application and periodically checks it
       """
-      try:
-         self.startTime = datetime.datetime.now()
-         self._initializeJob()
-         self._update()
-         cmd = self._buildCommand()
-         self._startApplication(cmd)
-         self.status = JobStatus.RUNNING
-         hbTime = 0
-         while not self._poll():
-            self._checkApplication()
-            if hbTime <= 0:
-               hbTime = HEARTBEAT_SECONDS
-               self._update()
-            else:
-               hbTime = hbTime - WAIT_SECONDS
-            self._wait()
-         self._checkApplication()
-         self._checkOutput()
-         self._update()
-         self.endTime = datetime.datetime.now()
-         self.metrics['start time'] = str(self.startTime)
-         self.metrics['end time'] = str(self.endTime)
-         self.metrics['elapsed time'] = str(self.endTime - self.startTime)
-         self._push()
-         if self.status < JobStatus.GENERAL_ERROR:
-            self.status = JobStatus.PUSH_COMPLETE
-         self._cleanUp()
-         self.writeMetrics(self.PROCESS_TYPE, self.job.jobId)
-         self.env.finalizeJob(self.job.processType, self.job.jobId)
-         if self.status < JobStatus.GENERAL_ERROR:
-            self.status = JobStatus.COMPLETE
-      except LmException, lme:
-         try:
-            self.log.debug(str(lme))
-         except: # Logger not initialized
-            print("Logging not initialized (early fail)")
-            print(str(lme))
-         self.status = lme.code
-         self._update()
-      except Exception, e:
-         try:
-            self.log.debug(str(e))
-         except: # Logger not initialized
-            print("Logging not initialized (early fail)")
-            print(str(e))
-         self.status = JobStatus.UNKNOWN_CLUSTER_ERROR
-         self._update()
-         raise e
+      cmd = self._buildCommand()
+      self._startApplication(cmd)
+      hbTime = 0
+      while not self._poll():
+         self._wait()
+      self._checkOutput()
 
    # ...................................
    def _buildCommand(self):
@@ -124,14 +66,6 @@ class ApplicationRunner(JobRunner):
       return ""
    
    # ...................................
-   def _checkApplication(self):
-      """
-      @summary: Checks a running application to get an updated progress and 
-                   status
-      """
-      return True
-   
-   # ...................................
    def _checkOutput(self):
       """
       @summary: Checks the output of an application to see if any unexpected
@@ -139,19 +73,6 @@ class ApplicationRunner(JobRunner):
       """
       pass
    
-   # ...................................
-   def _getMetricsAsStringIO(self):
-      """
-      @summary: Return all of the metrics as a StringIO object that can be 
-                   written to a zip file for return
-      """
-      val = '\n'.join(["{key}: {value}".format(key=k, value=self.metrics[k]
-                                               ) for k in self.metrics.keys()])
-      print(val)
-      s = StringIO(val)
-      s.seek(0)
-      return s
-      
    # ...................................
    def _poll(self):
       """
@@ -165,13 +86,6 @@ class ApplicationRunner(JobRunner):
          return False
    
    # ...................................
-   def _push(self):
-      """
-      @summary: Posts the results of the job back to the server
-      """
-      pass
-   
-   # ...................................
    def _startApplication(self, cmd):
       """
       @summary: This method takes a bash command and starts up an application.
@@ -180,13 +94,6 @@ class ApplicationRunner(JobRunner):
       sleep(WAIT_SECONDS)
    
    # ...................................
-   def _update(self):
-      """
-      @summary: Updates the job object in storage
-      """
-      print(self.env.updateJob(self.job.processType, self.job.jobId, self.status, self.progress))
-      
-   # ...................................
    def _wait(self):
       """
       @summary: Waits some amount of time so that the job runner isn't 
@@ -194,14 +101,3 @@ class ApplicationRunner(JobRunner):
       """
       sleep(WAIT_SECONDS)
       
-   # ..................................
-   def remoteStop(self):
-      """
-      @summary: This is called when a job is asked to stop remotely with 
-                   SIGTERM.  The job should do whatever it can to stop its 
-                   operation cleanly.  This signal will likely be followed by a 
-                   SIGKILL if emitted by a scheduler and that cannot be caught.
-      """
-      self.status = JobStatus.REMOTE_KILL
-      self._update()
-      sys.exit(1)
