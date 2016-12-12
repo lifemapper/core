8c2604d5c6a06aef6f02e940d5dc326e17a144c7
CJ Grady <cjgrady@gmail.com>
2016-12-12 16:04:41 -0500

    Module documentation and fixed imports

--- LmCommon/encoding/lmTree.py
+++ LmCommon/encoding/lmTree.py
@@ -1,12 +1,38 @@
-import os
-import cPickle
-import simplejson as json
-from itertools import combinations
-from operator import itemgetter
-#from lifemapperTools.common.NwkToJSON import Parser
-from NwkToJSON import Parser
+"""
+@summary: Module containing LMTree class
+@author: Jeff Cavner (edited by CJ Grady)
+@version: 1.0
+@status: alpha
+
+@license: gpl2
+@copyright: Copyright (C) 2017, University of Kansas Center for Research
+
+          Lifemapper Project, lifemapper [at] ku [dot] edu, 
+          Biodiversity Institute,
+          1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA
+   
+          This program is free software; you can redistribute it and/or modify 
+          it under the terms of the GNU General Public License as published by 
+          the Free Software Foundation; either version 2 of the License, or (at 
+          your option) any later version.
+  
+          This program is distributed in the hope that it will be useful, but 
+          WITHOUT ANY WARRANTY; without even the implied warranty of 
+          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+          General Public License for more details.
+  
+          You should have received a copy of the GNU General Public License 
+          along with this program; if not, write to the Free Software 
+          Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+          02110-1301, USA.
+"""
 import numpy as np
+from operator import itemgetter #TODO: Really necessary?
+import os
 from random import randint
+import simplejson as json # TODO: Do we need simplejson?
+
+from LmCommon.encoding.newickToJson import Parser
 
 
 
