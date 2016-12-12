e7772ca8b7b38a983782f104a58663b8960fcbe2
CJ Grady <cjgrady@gmail.com>
2016-12-12 16:17:34 -0500

    Top level module documentation

--- LmCompute/plugins/multi/mcpa/pValueCorrection.py
+++ LmCompute/plugins/multi/mcpa/pValueCorrection.py
@@ -4,6 +4,29 @@
 @see: Leibold, m.A., E.P. Economo and P.R. Peres-Neto. 2010. Metacommunity
          phylogenetics: separating the roles of environmental filters and 
          historical biogeography. Ecology letters 13: 1290-1299.
+@version: 1.0
+@status: alpha
+@license: gpl2
+@copyright: Copyright (C) 2017, University of Kansas Center for Research
+
+          Lifemapper Project, lifemapper [at] ku [dot] edu, 
+          Biodiversity Institute,
+          1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA
+   
+          This program is free software; you can redistribute it and/or modify 
+          it under the terms of the GNU General Public License as published by 
+          the Free Software Foundation; either version 2 of the License, or (at 
+          your option) any later version.
+  
+          This program is distributed in the hope that it will be useful, but 
+          WITHOUT ANY WARRANTY; without even the implied warranty of 
+          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+          General Public License for more details.
+  
+          You should have received a copy of the GNU General Public License 
+          along with this program; if not, write to the Free Software 
+          Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+          02110-1301, USA.
 """
 import numpy as np
 import os
