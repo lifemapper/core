3cca63961c6d7122c2568f39fd2ede8dda04668c
CJ Grady <cjgrady@gmail.com>
2017-02-14 12:25:32 -0500

    Rewrote OM requests to not rely on job XML

--- LmCompute/plugins/single/openModeller/omRunners.py
+++ LmCompute/plugins/single/openModeller/omRunners.py
@@ -9,7 +9,7 @@
 @note: Commands may be backwards compatible
 
 @license: gpl2
-@copyright: Copyright (C) 2016, University of Kansas Center for Research
+@copyright: Copyright (C) 2017, University of Kansas Center for Research
 
           Lifemapper Project, lifemapper [at] ku [dot] edu, 
           Biodiversity Institute,
@@ -29,26 +29,104 @@
           along with this program; if not, write to the Free Software 
           Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
           02110-1301, USA.
+@todo: Metrics keys
 """
+import json
+import ogr
 import os
+import shutil
+import time
+import zipfile
 
+from LmBackend.common.subprocessManager import SubprocessRunner
 from LmCommon.common.lmconstants import JobStatus, ProcessType
+from LmCompute.common.layerManager import LayerManager
 from LmCompute.common.lmObj import LmException
-from LmCompute.common.localconstants import BIN_PATH, JOB_DATA_PATH
+from LmCompute.common.lmconstants import LayerFormat
+from LmCompute.common.localconstants import SHARED_DATA_PATH
+from LmCompute.common.log import LmComputeLogger
 from LmCompute.jobs.runners.applicationRunner import ApplicationRunner
 from LmCompute.common.lmconstants import BIN_PATH
-from LmCompute.plugins.sdm.openModeller.constants import (OM_VERSION,
+from LmCompute.plugins.single.openModeller.constants import (OM_VERSION,
                                           DEFAULT_LOG_LEVEL, OM_MODEL_CMD, 
                                           OM_PROJECT_CMD)
-from LmCompute.plugins.sdm.openModeller.omRequest import (OmModelRequest, 
+from LmCompute.plugins.single.openModeller.omRequest import (OmModelRequest, 
                                                           OmProjectionRequest)
 
 # .............................................................................
-class OMModelRunner(ApplicationRunner):
+class OpenModellerModel(object):
    """
-   @summary: openModeller model job runner
+   @summary: Class containing methods to create an SDM model using openModeller
    """
-   PROCESS_TYPE = ProcessType.OM_MODEL
+   # ...................................
+   def __init__(self, jobName, pointsFn, layersFn, rulesetFn, paramsFn=None, 
+                packageFn=None, workDir=None, metricsFn=None, logFn=None, 
+                logLevel=None, statusFn=None):
+      """
+      @summary: Constructor for OM model
+      @param pointsFn: The file location of the shapefile containing points
+      @param layersFn: The file location of the JSON layers file
+      @param rulesetFn: The location to write the resulting ruleset
+      @param paramsFn: The JSON file location of algorithm parameters, if not
+                          provided, use defaults
+      @param packageFn: If provided, write the package output here
+      @param workDir: If provided, use this directory for work
+      @param metricsFn: If provided, write the metrics to this location
+      @param logFn: If provide, write the output log to this location
+      @param logLevel: The log level to use when logging
+      @param statusFn: If provided, write the status to this location
+      @todo: Assess
+      """
+      self.metrics = {}
+      self.metrics['processType'] = self.PROCESS_TYPE
+      # Process inputs
+      if workDir is not None:
+         self.workDir = workDir
+      else:
+         self.workDir = os.getcwd()
+
+      # Logs
+      if logFn is not None:
+         addFile=True
+      else:
+         addFile=False
+      self.log = LmComputeLogger(jobName, addConsole=True, addFile=addFile, 
+                                 logFilename=logFn)
+   
+      points, pointsWKT = self._processPoints(pointsFn)
+
+      # layers
+      layerFns, maskFn = self._processLayers(layersFn)
+
+      # parameters
+      algoParams = self._processParameters(paramsFn)
+      self.metrics['algorithmCode'] = self.algoCode
+      
+      # Command inputs
+      self.modelFile = os.path.join(self.workDir, 
+                                    "{0}-model.xml".format(jobName))
+      self.modelRequestFile = os.path.join(self.workDir, 
+                                           "{0}-request.xml".format(jobName))
+      self.modelLogFile = os.path.join(self.workDir, "{0}.log".format(jobName))
+      
+      # Output files
+      self.rulesetFn = rulesetFn
+      self.pkgFn = packageFn
+      self.metricsFn = metricsFn
+      self.statusFn = statusFn
+      
+      # Build request here
+      omr = OmModelRequest(points, jobName, pointsWKT, layerFns, algoParams,
+                           maskFn=maskFn)
+      with open(self.modelRequestFile, 'w') as reqF:
+         cnt = omr.generate()
+         # As of openModeller 1.3, need to remove <?xml ... first line
+         if cnt.startswith("<?xml version"):
+            tmp = cnt.split('\n')
+            cnt = '\n'.join(tmp[1:])
+
+         reqF.write(cnt)
+
    # ...................................
    def _buildCommand(self):
       """
@@ -67,29 +145,174 @@ class OMModelRunner(ApplicationRunner):
       """
       mdlBinary = os.path.join(BIN_PATH, OM_MODEL_CMD)
       cmd = "%s -r %s -m %s --log-level %s --log-file %s " % \
-               (mdlCmd, 
-                self.modelRequestFile, self.modelResultFile,
-                self.modelLogLevel, self.modelLogFile)
+               (mdlBinary, 
+                self.modelRequestFile, self.modelFile,
+                DEFAULT_LOG_LEVEL, self.modelLogFile)
 
       if not os.path.exists(mdlBinary):
-         self.status = JobStatus.LM_JOB_APPLICATION_NOT_FOUND
-         raise LmException(JobStatus.LM_JOB_APPLICATION_NOT_FOUND,  "%s not found" % mdlCmd)
+         raise LmException(JobStatus.LM_JOB_APPLICATION_NOT_FOUND,  
+                           "%s not found" % mdlBinary)
 
       return cmd
+
+   # ...................................
+   def _findError(self, stdErr):
+      """
+      @summary: Checks for information about the error
+      @param stdErr: Standard error from the process
+      """
+      status = JobStatus.OM_GENERAL_ERROR
+      
+      # Log standard error
+      self.log.debug("Checking standard error")
+      if stdErr is not None:
+         self.log.debug(stdErr)
+         # openModeller logs the error in a file.  Not sure what could be in 
+         #    standard error, but if something is found, check for it here
+         
+      self.log.debug("Checking output")
+      if os.path.exists(self.modelLogFile):
+         with open(self.modelLogFile) as logF:
+            omLog = logF.read()
+         
+         self.log.debug("openModeller error log")
+         self.log.debug("-----------------------")
+         self.log.debug(omLog)
+         self.log.debug("-----------------------")
+      
+         if omLog.find("[Error] No presence points available") >= 0:
+            status = JobStatus.OM_MOD_REQ_POINTS_MISSING_ERROR
+         elif omLog.find(
+             "[Error] Cannot use zero presence points for sampling") >= 0:
+            status = JobStatus.OM_MOD_REQ_POINTS_MISSING_ERROR
+         elif omLog.find("[Error] Algorithm could not be initialized") >= 0:
+            status = JobStatus.OM_MOD_REQ_POINTS_OUT_OF_RANGE_ERROR
+         elif omLog.find(
+             "[Error] Cannot create model without any presence or absence"
+             ) >= 0:
+            status = JobStatus.OM_MOD_REQ_POINTS_OUT_OF_RANGE_ERROR
+         elif omLog.find("[Error] XML Parser fatal error: not well-formed"
+                         ) >= 0:
+            status = JobStatus.OM_MOD_REQ_ERROR
+         elif omLog.find("[Error] Unable to open file") >= 0:
+            status = JobStatus.OM_MOD_REQ_LAYER_ERROR
+         elif omLog.find("[Error] Algorithm %s not found" % \
+                                               self.job.algorithm.code) >= 0:
+            status = JobStatus.OM_MOD_REQ_ALGO_INVALID_ERROR
+         elif omLog.find("[Error] Parameter") >= 0:
+            if omLog.find("not set properly.\n", 
+                                        omLog.find("[Error] Parameter")) >= 0:
+               status = JobStatus.OM_MOD_REQ_ALGOPARAM_MISSING_ERROR
+
+      return status
    
    # ...................................
-   def _checkOutput(self):
+   def _processLayers(self, layersFn):
       """
-      @summary: Checks the output of openModeller to see if any errors occurred
+      @summary: Read the layers JSON and process the layers accordingly
+      @param layersFn: Location of JSON file with layer information
+      @param layersDir: The directory to create layer symbolic links
       """
-      if self.progress == -2:
-         self.status = self._getModelErrorStatus()
-      elif self.progress == 100:
-         self.status = JobStatus.COMPUTED
-      else:
-         # Probably a seg fault or killed by signal
-         self.status = self._getModelErrorStatus()
+      with open(layersFn) as inLayers:
+         lyrJson = json.load(inLayers)
          
+      lyrMgr = LayerManager(SHARED_DATA_PATH)
+      lyrs, mask = lyrMgr.processLayersJSON(lyrJson, 
+                                 layerFormat=LayerFormat.GTIFF)
+      return lyrs, mask
+   
+   # .................................
+   def _processParameters(self, paramsFn):
+      """
+      @summary: Process provided algorithm parameters JSON and return string
+      @param paramsFn: File location of algorithm parameters JSON
+      @raise IOError: If algorithm JSON does not exist
+      @todo: Use constants
+      """
+      with open(paramsFn) as inParams:
+         paramsJson = json.load(inParams)
+         self.algoCode = paramsJson['algorithmCode']
+
+      return paramsJson
+      
+   # ...................................
+   def _processPoints(self, pointsShpFn):
+      """
+      @summary: Read the points and returns them as a list of tuples
+      @param pointsShpFn: The file location of the shapefile with points
+      """
+      points = []
+      driver = ogr.GetDriverByName("ESRI Shapefile")
+      dataSource = driver.Open(pointsShpFn, 0)
+      layer = dataSource.GetLayer()
+      
+      srs = layer.GetSpatialRef()
+      pointsWKT = srs.ExportToWkt()
+      
+      i = 1
+      for feature in layer:
+         geom = feature.GetGeometryRef()
+         ptGeom = geom.GetPoint()
+         # Append species name, x, y
+         points.append((i, ptGeom[0], ptGeom[1]))
+         i += 1
+
+      self.metrics['numPoints'] = len(points)
+      
+      return points, pointsWKT
+   
+   # ...................................
+   def run(self):
+      """
+      @summary: Run openModeller, create the model, write outputs appropriately
+      @todo: Assess
+      """
+      try:
+         cmd = self._buildCommand()
+         spr = SubprocessRunner(cmd)
+         startTime = time.time()
+         procExitStatus, procStdErr = spr.run()
+         endTime = time.time()
+         self.metrics['runningTime'] = endTime - startTime
+         status = JobStatus.COMPUTED
+         
+         # Check for output
+         if procExitStatus > 0 or not os.path.exists(self.modelFile):
+            # Error
+            status = self._findError(procStdErr)
+      except LmException, lme:
+         status = lme.status
+         
+      # Write outputs
+      # Ruleset
+      shutil.move(self.modelFile, self.rulesetFn)
+      
+      # Status
+      if self.statusFn is not None:
+         with open(self.statusFn, 'w') as outStatus:
+            outStatus.write(status)
+      
+      # Metrics
+      if self.metricsFn is not None:
+         with open(self.metricsFn, 'w') as outMetrics:
+            for k, v in self.metrics.iteritems():
+               outMetrics.write("{0}: {1}\n".format(k, v))
+      
+      # Package
+      if self.pkgFn is not None:
+         with zipfile.ZipFile(self.pkgFn, 'w', compression=zipfile.ZIP_DEFLATED,
+                               allowZip64=True) as zf:
+            for base, _, files in os.walk(self.workDir):
+               for f in files:
+                  zf.write(os.path.join(base, f), 
+                           os.path.relpath(os.path.join(base, f), self.workDir))
+
+class OMModelRunner(ApplicationRunner):
+   """
+   @summary: openModeller model job runner
+   """
+   PROCESS_TYPE = ProcessType.OM_MODEL
+   
    # ...................................
    def _processJobInput(self):
       """
