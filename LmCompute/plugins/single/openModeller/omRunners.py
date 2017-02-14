3a7ab2cce9d44172e997cb9f5cae3e1be53e6ed7
CJ Grady <cjgrady@gmail.com>
2017-02-14 12:40:15 -0500

    Rewrite of openModeller job management

--- LmCompute/plugins/single/openModeller/omRunners.py
+++ LmCompute/plugins/single/openModeller/omRunners.py
@@ -45,7 +45,6 @@ from LmCompute.common.lmObj import LmException
 from LmCompute.common.lmconstants import LayerFormat
 from LmCompute.common.localconstants import SHARED_DATA_PATH
 from LmCompute.common.log import LmComputeLogger
-from LmCompute.jobs.runners.applicationRunner import ApplicationRunner
 from LmCompute.common.lmconstants import BIN_PATH
 from LmCompute.plugins.single.openModeller.constants import (OM_VERSION,
                                           DEFAULT_LOG_LEVEL, OM_MODEL_CMD, 
@@ -265,7 +264,6 @@ class OpenModellerModel(object):
    def run(self):
       """
       @summary: Run openModeller, create the model, write outputs appropriately
-      @todo: Assess
       """
       try:
          cmd = self._buildCommand()
@@ -307,111 +305,79 @@ class OpenModellerModel(object):
                   zf.write(os.path.join(base, f), 
                            os.path.relpath(os.path.join(base, f), self.workDir))
 
-class OMModelRunner(ApplicationRunner):
-   """
-   @summary: openModeller model job runner
-   """
-   PROCESS_TYPE = ProcessType.OM_MODEL
-   
-   # ...................................
-   def _processJobInput(self):
-      """
-      @summary: Initializes a model to be ran by openModeller
-      """
-      self.metrics['jobId'] = self.job.jobId
-      self.metrics['processType'] = self.PROCESS_TYPE
-      self.metrics['algorithm'] = self.job.algorithm.code
-      self.metrics['numPoints'] = len(self.job.points.point)
-
-      self.modelLogFile = os.path.join(self.workDir, 'modLog-{0}.log'.format(
-                                                                 self.jobName))
-      self.modelRequestFile = os.path.join(self.workDir, 
-                                         'modReq-{0}.xml'.format(self.jobName))
-      self.modelResultFile = os.path.join(self.workDir, 'mod-{0}.xml'.format(
-                                                                 self.jobName))
-
-      self.log.debug("openModeller Version: %s" % OM_VERSION)
-      self.log.debug("-------------------------------------------------------")
-
-      self.modelLogLevel = DEFAULT_LOG_LEVEL
-      
-      self.log.debug("Acquiring inputs")
-      # Generate a model request file and write it to the file system
-      req = OmModelRequest(self.job, self.workDir)
-      reqFile = open(self.modelRequestFile, "w")
-      cnt = req.generate()
-      self.log.debug("Inputs acquired")
-     
-      # At least as of openModeller 1.3, need to remove the xml version line
-      if cnt.startswith("<?xml version"):
-         tmp = cnt.split('\n')
-         cnt = '\n'.join(tmp[1:])
-      reqFile.write(cnt)
-      reqFile.close()
-   
-   # .......................................
-   def _getModelErrorStatus(self):
-      """
-      @summary: Checks the model log file to determine what error occurred. 
-      """
-      f = open(self.modelLogFile)
-      omLog = ''.join(f.readlines())
-      f.close()
-      
-      print("openModeller error log")
-      print("-----------------------")
-      print(omLog)
-      print("-----------------------")
-      
-      status = JobStatus.UNKNOWN_ERROR
-      
-      if omLog.find("[Error] No presence points available") >= 0:
-         status = JobStatus.OM_MOD_REQ_POINTS_MISSING_ERROR
-      elif omLog.find(
-          "[Error] Cannot use zero presence points for sampling") >= 0:
-         status = JobStatus.OM_MOD_REQ_POINTS_MISSING_ERROR
-      elif omLog.find("[Error] Algorithm could not be initialized") >= 0:
-         status = JobStatus.OM_MOD_REQ_POINTS_OUT_OF_RANGE_ERROR
-      elif omLog.find(
-          "[Error] Cannot create model without any presence or absence point."
-          ) >= 0:
-         status = JobStatus.OM_MOD_REQ_POINTS_OUT_OF_RANGE_ERROR
-      elif omLog.find("[Error] XML Parser fatal error: not well-formed") >= 0:
-         status = JobStatus.OM_MOD_REQ_ERROR
-      elif omLog.find("[Error] Unable to open file") >= 0:
-         status = JobStatus.OM_MOD_REQ_LAYER_ERROR
-      elif omLog.find("[Error] Algorithm %s not found" % \
-                                            self.job.algorithm.code) >= 0:
-         status = JobStatus.OM_MOD_REQ_ALGO_INVALID_ERROR
-      elif omLog.find("[Error] Parameter") >= 0:
-         if omLog.find("not set properly.\n", 
-                                         omLog.find("[Error] Parameter")) >= 0:
-            status = JobStatus.OM_MOD_REQ_ALGOPARAM_MISSING_ERROR
-      return status
-   
-   # .......................................
-   def _finishJob(self):
-      """
-      @summary: Move outputs we want to keep to the specified location
-      @todo: Determine if anything else should be moved
-      @todo: Should we take a name parameter?
-      @todo: Move entire model package?
-      """
-      # Options to keep:
-      #  self.modelLogFile
-      #  self.modelRequestFile
-      #  metrics
-      
-      if self.outDir is not None:
-         shutil.move(self.modelResultFile, self.outDir)
-      
-   
 # .............................................................................
-class OMProjectionRunner(ApplicationRunner):
+class OpenModellerProjection(object):
    """
-   @summary: openModeller projection job runner
+   @summary: Class containing methods to create an SDM projection using 
+                openModeller
    """
    PROCESS_TYPE = ProcessType.OM_PROJECT
+   
+   # ...................................
+   def __init__(self, jobName, rulesetFn, layersFn, outTiffFn, workDir=None, 
+                metricsFn=None, logFn=None, logLevel=None, statusFn=None,
+                packageFn=None):
+      """
+      @summary: Constructor for ME model
+      @param pointsFn: The file location of the shapefile containing points
+      @param layersFn: The file location of the JSON layers file
+      @param rulesetFn: The location to write the resulting ruleset
+      @param paramsFn: The JSON file location of algorithm parameters, if not
+                          provided, use defaults
+      @param workDir: If provided, use this directory for work
+      @param metricsFn: If provided, write the metrics to this location
+      @param logFn: If provide, write the output log to this location
+      @param logLevel: The log level to use when logging
+      @param statusFn: If provided, write the status to this location
+      @todo: Assess
+      """
+      self.metrics = {}
+      self.metrics['algorithmCode'] = 'ATT_MAXENT'
+      self.metrics['processType'] = self.PROCESS_TYPE
+      # Process inputs
+      if workDir is not None:
+         self.workDir = workDir
+      else:
+         self.workDir = os.getcwd()
+
+      # Logs
+      if logFn is not None:
+         addFile=True
+      else:
+         addFile=False
+      self.log = LmComputeLogger(jobName, addConsole=True, addFile=addFile, 
+                                 logFilename=logFn)
+   
+      # layers
+      layerFns, maskFn = self._processLayers(layersFn)
+      
+      # Other
+      self.outTiffFn = outTiffFn
+      self.metricsFn = metricsFn
+      self.statusFn = statusFn
+      self.pkgFn = packageFn
+
+      # OM files
+      self.projRequestFile = os.path.join(self.workDir, 
+                                          "{0}-request.xml".format(jobName))
+      self.projectionFile = os.path.join(self.workDir, 
+                                         "{0}.tif".format(jobName))
+      self.projLogFile = os.path.join(self.workDir, "{0}.log".format(jobName))
+      self.projStatsFile = os.path.join(self.workDir, 
+                                        "{0}.stats".format(jobName))
+      
+      # Build request
+      omr = OmProjectionRequest(rulesetFn, layerFns, maskFn=maskFn)
+      
+      with open(self.projRequestFile, 'w') as reqF:
+         cnt = omr.generate()
+         # As of openModeller 1.3, need to remove <?xml ... first line
+         if cnt.startswith("<?xml version"):
+            tmp = cnt.split('\n')
+            cnt = '\n'.join(tmp[1:])
+
+         reqF.write(cnt)
+      
    # .......................................
    def _buildCommand(self):
       """
@@ -434,94 +400,100 @@ class OMProjectionRunner(ApplicationRunner):
       """
       prjBinary = os.path.join(BIN_PATH, OM_PROJECT_CMD)
       cmd = "%s -r %s -m %s --log-level %s --log-file %s --stat-file %s" % \
-            (prjBinary, self.projRequestFile, self.projResultFile,
-             self.projLogLevel, self.projLogFile, self.projStatFile)
+            (prjBinary, self.projRequestFile, self.projectionFile,
+             DEFAULT_LOG_LEVEL, self.projLogFile, self.projStatsFile)
       
       if not os.path.exists(prjBinary):
          self.status = JobStatus.LM_JOB_APPLICATION_NOT_FOUND
-         raise LmException(JobStatus.LM_JOB_APPLICATION_NOT_FOUND,  "%s not found" % prjCmd)
+         raise LmException(JobStatus.LM_JOB_APPLICATION_NOT_FOUND,  
+                           "%s not found" % prjBinary)
 
       return cmd
-   
-   # .......................................
-   def _checkOutput(self):
+
+   # ...................................
+   def _findError(self, stdErr):
       """
-      @summary: Checks the output of openModeller to see if any errors occurred
+      @summary: Checks for information about the error
+      @param stdErr: Standard error produced by the process
       """
-      if self.progress == -2:
-         self.status = self._getProjectionErrorStatus()
-      elif self.progress == 100:
-         self.status = JobStatus.COMPUTED
-      else:
-         # Probably a seg fault or killed by signal
-         self.status = self._getProjectionErrorStatus()
-   
-   # .......................................
-   def _getProjectionErrorStatus(self):
-      """
-      @summary: Checks the projection log file to determine what error occurred
-      @todo: Look for errors in log file
-      """
-      f = open(self.projLogFile)
-      omLog = ''.join(f.readlines())
-      f.close()
-      print("openModeller error log")
-      print("-----------------------")
-      print(omLog)
-      print("-----------------------")
-      
-      # Need to look for specific projection errors
       status = JobStatus.OM_PROJECTION_ERROR
       
+      # Log standard error
+      self.log.debug("Checking standard error")
+      if stdErr is not None:
+         self.log.debug(stdErr)
+         # No specific errors are known to present in standard error
+         
+      self.log.debug("Checking output")
+      
+      # Add the openModeller log
+      if os.path.exists(self.projLogFile):
+         with open(self.projLogFile, 'r') as f:
+            logContent = f.read()
+         self.log.debug("---------------------------------------")
+         self.log.debug(logContent)
+         self.log.debug("---------------------------------------")
+
       return status
    
-   # .......................................
-   def _processJobInput(self):
+   # ...................................
+   def _processLayers(self, layersFn):
       """
-      @summary: Initializes a projection for generation
+      @summary: Read the layers JSON and process the layers accordingly
+      @param layersFn: Location of JSON file with layer information
+      @param layersDir: The directory to create layer symbolic links
       """
-      self.metrics['jobId'] = self.job.jobId
-      self.metrics['processType'] = self.PROCESS_TYPE
-
-
-      self.projLogFile = "%s/projLog-%s.log" % (self.outputPath, self.job.jobId)
-      self.projLogLevel = DEFAULT_LOG_LEVEL
-      self.projProgressFile = "%s/projProg-%s.txt" % (self.outputPath, 
-                                                                self.job.jobId)
-      self.projRequestFile = "%s/projReq-%s.xml" % (self.outputPath, 
-                                                                self.job.jobId)
-      self.projResultFile = "%s/proj-%s.tif" % (self.outputPath, self.job.jobId)
-      self.projStatFile = "%s/projStats-%s.txt" % (self.outputPath, 
-                                                                self.job.jobId)
+      with open(layersFn) as inLayers:
+         lyrJson = json.load(inLayers)
+         
+      lyrMgr = LayerManager(SHARED_DATA_PATH)
+      lyrs, mask = lyrMgr.processLayersJSON(lyrJson, 
+                                 layerFormat=LayerFormat.GTIFF)
+      return lyrs, mask
+   
+   # ...................................
+   def run(self):
+      """
+      @summary: Run openModeller, create the projection, write outputs 
+                   appropriately
+      @todo: Assess
+      """
+      try:
+         cmd = self._buildCommand()
+         spr = SubprocessRunner(cmd)
+         startTime = time.time()
+         procExitStatus, procStdErr = spr.run()
+         endTime = time.time()
+         self.metrics['runningTime'] = endTime - startTime
+         status = JobStatus.COMPUTED
+         
+         # Check outputs            
+         if procExitStatus > 0 or not os.path.exists(self.asciiOut):
+            # Error
+            status = self._findError(procStdErr)
+      except LmException, lme:
+         status = lme.status
+         
+      # Write outputs
+      # Move projection output to proper location
+      shutil.move(self.projectionFile, self.outTiffFn)
       
-      self.log.debug("Acquiring inputs")
-      # Generate a projection request file and write it to the file system
-      req = OmProjectionRequest(self.job, JOB_DATA_PATH)
-      reqFile = open(self.projRequestFile, "w")
-      cnt = req.generate()
-      self.log.debug("Inputs acquired")
-     
-      # At least as of openModeller 1.3, need to remove the xml version line
-      if cnt.startswith("<?xml version"):
-         tmp = cnt.split('\n')
-         cnt = '\n'.join(tmp[1:])
-      reqFile.write(cnt)
-      reqFile.close()
+      # Status
+      if self.statusFn is not None:
+         with open(self.statusFn, 'w') as outStatus:
+            outStatus.write(status)
+      
+      # Metrics
+      if self.metricsFn is not None:
+         with open(self.metricsFn, 'w') as outMetrics:
+            for k, v in self.metrics.iteritems():
+               outMetrics.write("{0}: {1}\n".format(k, v))
 
-   # .......................................
-   def _finishJob(self):
-      """
-      @summary: Move outputs we want to keep to the specified location
-      @todo: Determine if anything else should be moved
-      @todo: Should we take a name parameter?
-      @todo: Move entire projection package?
-      """
-      # Options to keep:
-      #  self.projLogFile
-      #  self.projRequestFile
-      #  self.projStatFile
-      #  metrics
-      
-      if self.outDir is not None:
-         shutil.move(self.projResultFile, self.outDir)
-      
+      # Package
+      if self.pkgFn is not None:
+         with zipfile.ZipFile(self.pkgFn, 'w', compression=zipfile.ZIP_DEFLATED,
+                               allowZip64=True) as zf:
+            for base, _, files in os.walk(self.workDir):
+               for f in files:
+                  zf.write(os.path.join(base, f), 
+                           os.path.relpath(os.path.join(base, f), self.workDir))
