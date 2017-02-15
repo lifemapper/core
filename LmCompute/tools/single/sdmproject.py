d98aebf5ab7d1445df78d3758cdb3256ec1dec6e
CJ Grady <cjgrady@gmail.com>
2017-02-15 10:18:00 -0500

    Updated to match changes in projection tools

--- LmCompute/tools/single/sdmproject.py
+++ LmCompute/tools/single/sdmproject.py
@@ -27,8 +27,8 @@
 """
 import argparse
 from LmCommon.common.lmconstants import ProcessType
-from LmCompute.plugins.single.maxent.meRunners import MEProjectionRunner
-from LmCompute.plugins.single.openModeller.omRunners import OMProjectionRunner
+from LmCompute.plugins.single.maxent.meRunners import MaxentProjection
+from LmCompute.plugins.single.openModeller.omRunners import OpenModellerProjection
 
 # .............................................................................
 if __name__ == "__main__":
@@ -37,43 +37,45 @@ if __name__ == "__main__":
    parser = argparse.ArgumentParser(
       description='This script generates a SDM projection from Lifemapper inputs') 
    
-   parser.add_argument('-n', '--job_name', type=str,
-                       help='Use this as the name of the job (for logging and work directory creation).  If omitted, one will be generated')
-   parser.add_argument('-p', '--process_type', type=int,
-                       choices=[ProcessType.ATT_PROJECT, ProcessType.OM_PROJECT],
-                       help='Type of SDM to run, ProcessType.ATT_MODEL or ProcessType.OM_MODEL')
-   parser.add_argument('-o', '--out_dir', type=str, 
-                       help='Write the final outputs to this directory')
+   parser.add_argument('processType', type=int, 
+                     choices=[ProcessType.ATT_PROJECT, ProcessType.OM_PROJECT],
+                     help="The Lifemapper process type of this projection")
+   parser.add_argument('jobName', type=str, help="A name for this projection")
+   parser.add_argument('rulesetFn', type=str, 
+                       help="File location of the model ruleset to use")
+   parser.add_argument('layersFn', type=str, 
+          help="File location of JSON document containing layer information for projecting")
+   parser.add_argument('outputRaster', type=str, 
+                       help="File location to write the resulting raster")
+   
+   # Optional arguments
+   parser.add_argument('-algo', '--params_file', dest='params_file', type=str, 
+          help="File location of JSON document containing algorithm parameter information (only Maxent)")
    parser.add_argument('-w', '--work_dir', type=str, 
-                       help='The workspace directory where the work directory should be created.  If omitted, will use current directory')
-   parser.add_argument('-l', '--log_file', type=str, 
-                       help='Optional output logfile')
-   parser.add_argument('-ll', '--log_level', type=str, 
-                       help='Log level for optional logging', 
-                       choices=['info', 'debug', 'warn', 'error', 'critical'])
-   parser.add_argument('-s', '--status_file', type=str,
-                       help='Optional output job status file')
+                       help='Path for work directory creation. Defaults to current directory')
    parser.add_argument('--metrics_file', type=str, 
                        help='Optional output metrics file')
-   parser.add_argument('--cleanup', type=bool, 
-                       help='Clean up outputs or not', 
-                       choices=[True, False])
-   parser.add_argument('jobXml', type=str, 
-                       help='Job configuration information XML file')
+   parser.add_argument('-l', '--log_file', type=str, 
+                       help='Optional output logfile')
+   parser.add_argument('-s', '--status_file', type=str,
+                       help='Optional output job status file')
+   parser.add_argument('-p', '--package_file', type=str, 
+             help="If provided, write the projection package to this location")
    
    args = parser.parse_args()
    
-   if args.process_type == ProcessType.ATT_PROJECT:
-      job = MEProjectionRunner(args.jobXml, jobName=args.job_name, 
-                               outDir=args.out_dir, workDir=args.work_dir, 
-                               metricsFn=args.metrics_file, logFn=args.log_file, 
-                               logLevel=args.log_level, 
-                               statusFn=args.status_file)
+   if args.processType == ProcessType.ATT_PROJECT:
+      job = MaxentProjection(args.jobName, args.rulesetFn, args.layersFn, 
+                             args.outputRaster, paramsFn=args.params_file,
+                             workDir=args.work_dir, metricsFn=args.metrics_file,
+                             logFn=args.log_file, statusFn=args.status_file, 
+                             packageFn=args.package_file)
    else:
-      job = OMProjectionRunner(args.jobXml, jobName=args.job_name, 
-                               outDir=args.out_dir, workDir=args.work_dir, 
-                               metricsFn=args.metrics_file, logFn=args.log_file, 
-                               logLevel=args.log_level, 
-                               statusFn=args.status_file)
+      job = OpenModellerProjection(args.jobName, args.rulesetFn, args.layersFn, 
+                             args.outputRaster, workDir=args.work_dir, 
+                             metricsFn=args.metrics_file, logFn=args.log_file, 
+                             statusFn=args.status_file, 
+                             packageFn=args.package_file)
+      
    job.run()
    
