f02711d749f6ccd0cf9bea52a05a9105ae894330
CJ Grady <cjgrady@gmail.com>
2017-02-15 14:11:04 -0500

    Stubbed out update object script

--- LmCompute/tools/common/updateObjectStatus.py
+++ LmCompute/tools/common/updateObjectStatus.py
@@ -1,7 +1,6 @@
 #!/bin/bash
 """
 @summary: This script updates a Lifemapper object in the database
-@author: CJ Grady
 @version: 4.0.0
 @status: beta
 
@@ -26,8 +25,6 @@
           along with this program; if not, write to the Free Software 
           Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
           02110-1301, USA.
-@note: If no indices mapping file is provided, assume that the tree already has 
-          matrix indices in it
 """
 import argparse
 
@@ -38,8 +35,17 @@ from LmServer.db.scribe import Scribe
 if __name__ == "__main__":
    parser = argparse.ArgumentParser(description="This script updates a Lifemapper object")
    # Inputs
-   parser.add_argument("processType", help="The process type of the object to update")
-   parser.add_argument("objectId", help="The id of the object to update")
+   parser.add_argument("processType", type=str, 
+                       help="The process type of the object to update")
+   parser.add_argument("objectId", type=str, 
+                       help="The id of the object to update")
+   parser.add_argument("successFile", type=str, 
+            help="This file is created if the job was completed successfully.  Otherwise, it is not.")
+
+   parser.add_argument("objectOutput", type=str, nargs="*", 
+            help="These files are the outputs for an object to sanity check.  Each process type should know how many there are")
+
+   # Status arguments
    parser.add_argument("-s", dest="status", type=int, 
                        help="The status to update the object with")
    parser.add_argument("-f", dest="status_file", type=str, 
@@ -54,10 +60,22 @@ if __name__ == "__main__":
          status = int(statusIn.read())
 
    if status is not None:
-      raise NotImplementedError("The update method does not exist yet")
       scribe = Scribe(ConsoleLogger())
       scribe.openConnections()
-      scribe.updateObjectStatus(args.processType, args.objectId, status)
+      
+      success = False
+      # Aimee: Handle each object type here
+      
+      # args.objectOutput will contain file paths for created files
+      
+      if success:
+         # Only write success file if successfully updated an object with 
+         #    non-error status.  Otherwise, Makeflow should stop and that will 
+         #    happen without this file
+         with open(args.successFile, 'w') as successOut:
+            successOut.write('1')
+      
       scribe.closeConnections()
    else:
       raise Exception("Must provide status or status file")
+   
