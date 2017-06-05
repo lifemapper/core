0113a7c7ffe4a87c5763ba7ae8009b77e0248a48
root <root@notyeti-191.lifemapper.org>
2017-06-05 16:33:03 -0500

    Removed lmFormat wrapper around POST (for now since we don't return anything)

--- LmWebServer/services/api/v2/sdmProject.py
+++ LmWebServer/services/api/v2/sdmProject.py
@@ -115,7 +115,7 @@ class SdmProjectService(LmService):
    # ................................
    #@cherrypy.tools.json_in
    #@cherrypy.tools.json_out
-   @lmFormatter
+   #@lmFormatter
    def POST(self):
       """
       @summary: Posts a new projection
