f4baab2e7efc01e48127df90de535b9e70f06257
CJ Grady <cjgrady@gmail.com>
2017-06-08 16:09:34 -0400

    Added solr post command

--- LmServer/legion/mtxcolumn.py
+++ LmServer/legion/mtxcolumn.py
@@ -229,6 +229,20 @@ class MatrixColumn(Matrix, _LayerParameters, ServiceObject, ProcessObject):
          rules.append(uRule)
          
          # TODO: Post to Solr
+         postXmlFilename = os.path.join(targetDir, 
+                                    'mtxcol_solr_{}.xml'.format(self.getId()))
+         solrPostArgs = [
+            'LOCAL',
+            '$PYTHON',
+            pavFname,
+            self.getId(), # PAV id
+            self.layer.getId(), # Projection id
+            self.layer.occurrenceSet.getId(),
+            postXmlFilename
+         ]
+         solrCmd = ' '.join(solrPostArgs)
+         solrRule = MfRule(solrCmd, [postXmlFilename], dependencies=[pavFname])
+         rules.append(solrRule)
          
 
       return rules
