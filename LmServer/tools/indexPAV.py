2cac6a52f81824ba8c7e19ecd107339641f675d0
CJ Grady <cjgrady@gmail.com>
2017-06-21 13:51:30 -0400

    Use getshapegrid to get shapegrid

--- LmServer/tools/indexPAV.py
+++ LmServer/tools/indexPAV.py
@@ -47,7 +47,7 @@ def getPostDocument(pav, prj, occ, pam, sciName, pavFname):
    @param occ: An occurrence layer object
    @return: A string that can be posted to the Solr index
    """
-   sg = pam.shapegrid
+   sg = pam.getShapegrid()
    mdlScn = prj.modelScenario
    prjScn = prj.projScenario
    pavMtx = Matrix.load(pavFname)
