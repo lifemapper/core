a35c5f64ab8023c45fde941063a4822480cbdf8a
CJ Grady <cjgrady@gmail.com>
2017-07-12 15:49:13 -0400

    Bumped up the number of permutations for MCPA to test results (still very low)

--- LmServer/legion/gridset.py
+++ LmServer/legion/gridset.py
@@ -37,8 +37,8 @@ from LmServer.legion.lmmatrix import LMMatrix
 from LmServer.legion.cmd import MfRule
 
 # TODO: Move these to localconstants
-NUM_RAND_GROUPS = 5
-NUM_RAND_PER_GROUP = 5
+NUM_RAND_GROUPS = 20
+NUM_RAND_PER_GROUP = 20
 
 
 # .............................................................................
