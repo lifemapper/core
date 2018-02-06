89c5a8c7789741f9a55305d82a5e29e36218f349
CJ Grady <cjgrady@gmail.com>
2018-02-06 10:10:54 -0500

    Wrapped all makeflow commands to ensure they complete successfully

---
+++ LmBackend/tools/lm_wrapper.py
@@ -0,0 +1,51 @@
+"""
+@summary: This class wraps a command and ensures that the outputs are always
+             created
+@author: CJ Grady
+@license: gpl2
+@copyright: Copyright (C) 2018, University of Kansas Center for Research
+
+          Lifemapper Project, lifemapper [at] ku [dot] edu, 
+          Biodiversity Institute,
+          1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA
+   
+          This program is free software; you can redistribute it and/or modify 
+          it under the terms of the GNU General Public License as published by 
+          the Free Software Foundation; either version 2 of the License, or (at 
+          your option) any later version.
+  
+          This program is distributed in the hope that it will be useful, but 
+          WITHOUT ANY WARRANTY; without even the implied warranty of 
+          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+          General Public License for more details.
+  
+          You should have received a copy of the GNU General Public License 
+          along with this program; if not, write to the Free Software 
+          Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+          02110-1301, USA.
+"""
+
+import argparse
+import os
+from subprocess import Popen
+from time import sleep
+
+from LmBackend.common.lmobj import LMObject
+
+# .............................................................................
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument('cmd', type=str, help='This is the command to be wrapped')
+   parser.add_argument('touch_files', type=str, nargs='*', 
+               help='These files will be created by this script if necessary')
+   args = parser.parse_args()
+   p = Popen(args.cmd, shell=True)
+   while p.poll() is None:
+      sleep(1)
+   
+   for fn in args.touch_files:
+      lmo = LMObject()
+      lmo.readyFilename(fn)
+      with open(fn, 'a') as outF:
+         os.utime(fn)
+         
