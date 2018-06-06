7016d27b8b5deaea2b51fa3eb2424e8d28e77edb
CJ Grady <cjgrady@gmail.com>
2018-06-06 16:00:22 -0400

    Add 'analysis' function for gridsets

--- LmServer/common/lmconstants.py
+++ LmServer/common/lmconstants.py
@@ -1522,3 +1522,15 @@ MASK_LAYER_KEY = 'mask_layer'
 MASK_LAYER_NAME_KEY = 'mask_name'
 PRE_PROCESS_KEY = 'preprocess'
 PROCESSING_KEY = 'processing'
+
+class SubsetMethod(object):
+   """
+   """
+   COLUMN = 0 # Same shapegrid, just cut out columns
+   SPATIAL = 1 # Spatial subset of original shapegrid.  Column and row, 
+               #    cut out columns and remove sites
+   REINTERSECT = 2 # New shapegrid with possibly different resolution, 
+   #                    reintersect all columns 
+   
+   
+   
