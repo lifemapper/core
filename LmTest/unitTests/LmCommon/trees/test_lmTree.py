403e4ba73dd632664bd8ab40cac7519c8a92c3ff
CJ Grady <cjgrady@gmail.com>
2018-07-03 09:11:35 -0400

    Deprecated tree unit tests

--- LmTest/unitTests/LmCommon/trees/test_lmTree.py
+++ LmTest/unitTests/LmCommon/trees/test_lmTree.py
@@ -25,6 +25,9 @@
           along with this program; if not, write to the Free Software 
           Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
           02110-1301, USA.
+          
+          
+@deprecated: This was from old tree module.  Need to completely rewrite
 """
 from copy import deepcopy
 import json
