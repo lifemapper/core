9072ecead900b1704adc5cbe26a393f352a975f0
CJ Grady <cjgrady@gmail.com>
2018-12-07 15:39:35 -0500

    Code style

--- LmBackend/command/boom.py
+++ LmBackend/command/boom.py
@@ -1,43 +1,22 @@
-"""
-@summary: This module contains command classes for BOOM processes
-@author: CJ Grady
-@license: gpl2
-@copyright: Copyright (C) 2018, University of Kansas Center for Research
-
-          Lifemapper Project, lifemapper [at] ku [dot] edu, 
-          Biodiversity Institute,
-          1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA
-   
-          This program is free software; you can redistribute it and/or modify 
-          it under the terms of the GNU General Public License as published by 
-          the Free Software Foundation; either version 2 of the License, or (at 
-          your option) any later version.
-  
-          This program is distributed in the hope that it will be useful, but 
-          WITHOUT ANY WARRANTY; without even the implied warranty of 
-          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
-          General Public License for more details.
-  
-          You should have received a copy of the GNU General Public License 
-          along with this program; if not, write to the Free Software 
-          Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
-          02110-1301, USA.
+"""This module contains command classes for BOOM processes
 """
 from LmBackend.command.base import _LmCommand
 from LmBackend.common.lmconstants import BOOM_SCRIPTS_DIR, CMD_PYBIN
 
 # .............................................................................
 class BoomerCommand(_LmCommand):
-    """
-    @summary: This command will run the boomer
+    """This command will run the boomer
     """
     relDir = BOOM_SCRIPTS_DIR
     scriptName = 'boomer.py'
 
     # ................................
     def __init__(self, configFile, successFile):
-        """
-        @summary: Construct the command object
+        """Constructs the command object
+
+        Args:
+            configFile (str) : The file path to a BOOM config file.
+            successFile (str) : The relative file path f
         @param configFile: Configuration file for the boom run
         """
         _LmCommand.__init__(self)
