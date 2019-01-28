b976a339de83d081c58d0b71fbeb600cb165e6cc
zzeppozz <aimee.stewart@ku.edu>
2019-01-28 11:47:43 -0600

    move command

--- LmBackend/command/boom.py
+++ LmBackend/command/boom.py
@@ -1,7 +1,11 @@
 """This module contains command classes for BOOM processes
 """
+import os
+import time
+
 from LmBackend.command.base import _LmCommand
 from LmBackend.common.lmconstants import BOOM_SCRIPTS_DIR, CMD_PYBIN
+from LmCommon.common.lmconstants import LMFormat
 
 # .............................................................................
 class BoomerCommand(_LmCommand):
@@ -36,4 +40,49 @@ class BoomerCommand(_LmCommand):
 
 
     
-    
+# .............................................................................
+class CatalogBoomCommand(_LmCommand):
+    """
+    @summary: This command will create makeflows to:
+                    * catalog boom archive inputs,
+                    * catalog ScenarioPackage if necessary
+                    * create GRIMs, 
+                    * create an archive ini file, and 
+                    * start the Boomer to walk through inputs
+    """
+    relDir = BOOM_SCRIPTS_DIR
+    scriptName = 'initBoomJob.py'
+
+    # ................................
+    def __init__(self, config_filename, init_makeflow=False):
+        """
+        @summary: Construct the command object
+        @param config_filename: The file location of the ini file 
+                 with parameters for a boom/gridset
+        """
+        _LmCommand.__init__(self)
+        
+        if not os.path.exists(config_filename):
+            raise Exception('Missing Boom configuration file {}'.format(config_filename))
+        else:
+            boomBasename, _ = os.path.splitext(os.path.basename(config_filename)) 
+            # Logfile goes to LOG_DIR
+            secs = time.time()
+            timestamp = "{}".format(time.strftime("%Y%m%d-%H%M", time.localtime(secs)))
+            logname = '{}.{}.{}'.format(self.scriptBasename, boomBasename, timestamp)
+            logfilename = '{}{}'.format(logname, LMFormat.LOG.ext)
+            
+        # Script args
+        self.args = config_filename
+        self.args += ' --logname={}'.format(logname)
+        if init_makeflow:
+            self.args += ' --init_makeflow=True'
+            
+        self.outputs.append(logfilename)
+            
+    # ................................
+    def getCommand(self):
+        """
+        @summary: Get the command
+        """
+        return '{} {} {}'.format(CMD_PYBIN, self.getScript(), self.args)
