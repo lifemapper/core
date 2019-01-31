28265ef1b716c221de76a904edc53b8b836e26ed
CJ Grady <cjgrady@gmail.com>
2019-01-31 10:31:40 -0600

    Code style, docs, bug fix

--- LmCompute/plugins/multi/randomize/swap.py
+++ LmCompute/plugins/multi/randomize/swap.py
@@ -1,30 +1,4 @@
-"""
-@summary: Module containing functions to randomize a RAD PAM
-@author: Lifemapper Team; lifemapper@ku.edu
-@version: 4.0.0
-@status: beta
-
-@license: gpl2
-@copyright: Copyright (C) 2019, University of Kansas Center for Research
-
-          Lifemapper Project, lifemapper [at] ku [dot] edu, 
-          Biodiversity Institute,
-          1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA
-   
-          This program is free software; you can redistribute it and/or modify 
-          it under the terms of the GNU General Public License as published by 
-          the Free Software Foundation; either version 2 of the License, or (at 
-          your option) any later version.
-  
-          This program is distributed in the hope that it will be useful, but 
-          WITHOUT ANY WARRANTY; without even the implied warranty of 
-          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
-          General Public License for more details.
-  
-          You should have received a copy of the GNU General Public License 
-          along with this program; if not, write to the Free Software 
-          Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
-          02110-1301, USA.
+"""Module containing Gotelli swap method for randomizing a PAM
 """
 from random import randrange
 
@@ -33,42 +7,48 @@ from LmCommon.common.matrix import Matrix
 MAX_TRIES_WITHOUT_SWAP = 1000000
 
 # .............................................................................
-def swapRandomize(matrix, numSwaps, maxTries=MAX_TRIES_WITHOUT_SWAP):
-   """
-   @summary: Randomize a compressed matrix using the Swap method
-   @param matrix: A PAM Matrix object
-   @param numSwaps: The number of successful swaps to perform
-   @param maxTries: The maximum number of tries to swap before failing
-   """
-   mtxHeaders = matrix.getHeaders()
-   swappedMtx = matrix.data.copy().astype(int)
-   counter = 0
-   numTries = 0
-   rowLen, colLen = matrix.data.shape
+def swapRandomize(matrix, num_swaps, max_tries=MAX_TRIES_WITHOUT_SWAP):
+    """Randomize a PAM matrix using the Gotelli swap method
 
-   #numTries is a safety to kill the loop if nothing is ever found
-   while counter < numSwaps and numTries < MAX_TRIES_WITHOUT_SWAP: 
-      numTries += 1
-      column1 = randrange(0, colLen)
-      column2 = randrange(0, colLen)
-      row1 = randrange(0, rowLen)                   
-      while column2 == column1:
-         column2 = randrange(0, colLen)             
-      firstcorner = swappedMtx[row1][column1]     
-      if firstcorner ^ swappedMtx[row1][column2]:
-         row2 = randrange(0, rowLen)              
-         while row2 == row1:
-               row2 = randrange(0, rowLen)
-         if ((firstcorner ^ swappedMtx[row2][column1]) and 
-             (not(firstcorner) ^ swappedMtx[row2][column2])):
-               swappedMtx[row1][column2] = firstcorner
-               swappedMtx[row2][column1] = firstcorner
-               swappedMtx[row2][column2] = not(firstcorner)
-               swappedMtx[row1][column1] = not(firstcorner)                 
-               counter += 1
-               numTries = 0
-   if numTries >= MAX_TRIES_WITHOUT_SWAP:
-      raise Exception("Reached maximum number of tries without finding suitable swap")
-      
-   return Matrix(swappedMtx, headers=mtxHeaders)
-   
+    Args:
+        matrix (:obj: Matrix): A Lifemapper matrix object with binary elements
+        num_swaps (:obj: int): The number of swaps to perform
+        max_tries (:obj: int): The maximum number of attempts to swap before
+            failing
+
+    Todo:
+        Allow num_swaps to be specified as a percentage of the matrix fill
+    """
+    mtx_headers = matrix.getHeaders()
+    swapped_mtx = matrix.data.copy().astype(int)
+    counter = 0
+    num_tries = 0
+    row_len, col_len = matrix.data.shape
+
+    #num_tries is a safety to kill the loop if nothing is ever found
+    while counter < num_swaps and num_tries < max_tries:
+        num_tries += 1
+        column1 = randrange(0, col_len)
+        column2 = randrange(0, col_len)
+        row1 = randrange(0, row_len)
+        while column2 == column1:
+            column2 = randrange(0, col_len)
+        first_corner = swapped_mtx[row1][column1]
+        if first_corner ^ swapped_mtx[row1][column2]:
+            row2 = randrange(0, row_len)
+            while row2 == row1:
+                    row2 = randrange(0, row_len)
+            if ((first_corner ^ swapped_mtx[row2][column1]) and
+                 (not(first_corner) ^ swapped_mtx[row2][column2])):
+                    swapped_mtx[row1][column2] = first_corner
+                    swapped_mtx[row2][column1] = first_corner
+                    swapped_mtx[row2][column2] = not(first_corner)
+                    swapped_mtx[row1][column1] = not(first_corner)
+                    counter += 1
+                    num_tries = 0
+    if num_tries >= max_tries:
+        raise Exception(
+            'Reached maximum number of tries without finding suitable swap')
+
+    return Matrix(swapped_mtx, headers=mtx_headers)
+    
