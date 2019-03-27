1615d3a6eafd6b0da92adbbb0a1c71a0d35154e4
CJ Grady <cjgrady@gmail.com>
2019-03-27 15:54:00 -0500

    Check to see if delimiter was included in post

--- LmWebServer/services/api/v2/upload.py
+++ LmWebServer/services/api/v2/upload.py
@@ -243,7 +243,8 @@ class UserUploadService(LmService):
                         field_obj['role'] = 'uniqueId'
                     meta_obj[field_idx] = field_obj
             
-                meta_obj['delimiter'] = metadata['delimiter']
+                if 'delimiter' in metadata.keys():
+                    meta_obj['delimiter'] = metadata['delimiter']
                 with open(metaFilename, 'w') as outF:
                     json.dump(meta_obj, outF)
         # Process file
