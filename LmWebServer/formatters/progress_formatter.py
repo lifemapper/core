0df81783357e35fb9b1532b47428df61cee2c9df
CJ Grady <cjgrady@gmail.com>
2019-04-02 11:13:37 -0500

    Use new functions for faster progress

--- LmWebServer/formatters/progress_formatter.py
+++ LmWebServer/formatters/progress_formatter.py
@@ -9,6 +9,31 @@ from LmServer.common.log import LmPublicLogger
 from LmServer.db.borgscribe import BorgScribe
 
 # .............................................................................
+def summarize_object_statuses(summary):
+    """Summarizes a summary
+
+    Args:
+        summary (:obj:`list` of :obj:`tuple` of :obj:`int`, :obj:`int`): A list
+            of (status, count) tuples for an object type
+    """
+    complete = 0
+    waiting = 0
+    running = 0
+    error = 0
+    total = 0
+    for status, count in summary:
+        if status <= JobStatus.INITIALIZE:
+            waiting += count
+        elif status <= JobStatus.COMPLETE:
+            running += count
+        elif status == JobStatus.COMPLETE:
+            complete += count
+        else:
+            error += count
+        total += count
+    return (waiting, running, complete, error, total)
+
+# .............................................................................
 def format_gridset(gridset_id, detail=False):
     """Returns a dictionary of progress information for a gridset
 
@@ -18,78 +43,53 @@ def format_gridset(gridset_id, detail=False):
     scribe = BorgScribe(LmPublicLogger())
     scribe.openConnections()
     
+    prj_summary = scribe.summarizeSDMProjectsForGridset(gridset_id)
+    mtx_summary = scribe.summarizeMatricesForGridset(gridset_id)
+    mf_summary = scribe.summarizeMFChainsForGridset(gridset_id)
+    mc_summary = scribe.summarizeMtxColumnsForGridset(gridset_id)
+    occ_summary = scribe.summarizeOccurrenceSetsForGridset(gridset_id)
+    
+    (waiting_prjs, running_prjs, complete_prjs, error_prjs, total_prjs
+     ) = summarize_object_statuses(prj_summary)
+    (waiting_mtxs, running_mtxs, complete_mtxs, error_mtxs, total_mtxs
+     ) = summarize_object_statuses(mtx_summary)
+    (waiting_mfs, running_mfs, complete_mfs, error_mfs, total_mfs
+     ) = summarize_object_statuses(mf_summary)
+    (waiting_occs, running_occs, complete_occs, error_occs, total_occs
+     ) = summarize_object_statuses(occ_summary)
+    (waiting_mcs, running_mcs, complete_mcs, error_mcs, total_mcs
+     ) = summarize_object_statuses(mc_summary)
+    scribe.closeConnections()
+    
+    # Progress is determined by makeflows.  If all SDMs error, then -1
+    if error_occs == total_occs and total_occs > 0:
+        progress = -1.0
+    elif error_prjs == total_prjs and total_prjs > 0:
+        progress = -1.0
+    else:
+        # 0.5 * number running + 1.0 * number complete + number error / total
+        progress = (
+            0.5 * running_mfs + 1.0 * (complete_mfs + error_mfs)) / total_mfs
+    
     if detail:
-        gridset = scribe.getGridset(gridsetId=gridset_id)
-        complete_mtxs = 0
-        error_mtxs = 0
-        running_mtxs = 0
-        waiting_mtxs = 0
-
-        # Gridset object has matrix objects so we can count status
-        complete_mtxs = scribe.countMatrices(
-            userId=gridset.getUserId(), gridsetId=gridset_id,
-            afterStatus=JobStatus.COMPLETE - 1,
-            beforeStatus=JobStatus.COMPLETE + 1)
-        waiting_mtxs = scribe.countMatrices(
-            userId=gridset.getUserId(), gridsetId=gridset_id,
-            afterStatus=JobStatus.GENERAL - 1,
-            beforeStatus=JobStatus.GENERAL + 1)
-        running_mtxs = scribe.countMatrices(
-            userId=gridset.getUserId(), gridsetId=gridset_id,
-            afterStatus=JobStatus.GENERAL + 1,
-            beforeStatus=JobStatus.COMPLETE - 1)
-        error_mtxs = scribe.countMatrices(
-            userId=gridset.getUserId(), gridsetId=gridset_id,
-            afterStatus=JobStatus.GENERAL_ERROR - 1)
-        
-        # Use scribe to get counts for projections
-        
-        complete_prjs = scribe.countSDMProjects(
-            userId=gridset.getUserId(), afterStatus=JobStatus.COMPLETE - 1,
-            beforeStatus=JobStatus.COMPLETE + 1, gridsetId=gridset.getId()) 
-        error_prjs = scribe.countSDMProjects(
-            userId=gridset.getUserId(), afterStatus=JobStatus.GENERAL_ERROR - 1,
-            gridsetId=gridset.getId())
-        running_prjs = scribe.countSDMProjects(
-            userId=gridset.getUserId(), afterStatus=JobStatus.GENERAL + 1,
-            beforeStatus=JobStatus.COMPLETE - 1, gridsetId=gridset.getId())
-        waiting_prjs = scribe.countSDMProjects(
-            userId=gridset.getUserId(), beforeStatus=JobStatus.GENERAL + 1,
-            gridsetId=gridset.getId())
-        
-        total_prjs = complete_prjs + error_prjs + running_prjs + waiting_prjs
-        
-        complete_wfs = scribe.countMFChains(
-            gridsetId=gridset.getId(), afterStat=JobStatus.COMPLETE - 1,
-            beforeStat=JobStatus.COMPLETE + 1)
-        error_wfs = scribe.countMFChains(
-            gridsetId=gridset.getId(), afterStat=JobStatus.GENERAL_ERROR - 1)
-        running_wfs = scribe.countMFChains(
-            gridsetId=gridset.getId(), afterStat=JobStatus.GENERAL + 1,
-            beforeStat=JobStatus.COMPLETE - 1)
-        waiting_wfs = scribe.countMFChains(
-            gridsetId=gridset.getId(), beforeStat=JobStatus.GENERAL + 1)
-        
-        scribe.closeConnections()
-        
-        # Initialize in case there are zero prj or mtx
-        mtx_percent = 1.0
-        prj_percent = 1.0
-        
-        if len(gridset.getMatrices()) > 0:
-            mtx_percent = 1.0 * (
-                complete_mtxs + error_mtxs) / len(gridset.getMatrices())
-        if total_prjs > 0:
-            prj_percent = 1.0 * (complete_prjs + error_prjs) / total_prjs
-        
-        progress = 0.5 * (mtx_percent + prj_percent)
-        
         progress_dict = {
             'matrices' : {
                 'complete' : complete_mtxs,
                 'error' : error_mtxs,
                 'running' : running_mtxs,
                 'waiting' : waiting_mtxs
+            },
+            'matrix_columns' : {
+                'complete' : complete_mcs,
+                'error' : error_mcs,
+                'running' : running_mcs,
+                'waiting' : waiting_mcs
+            },
+            'occurrences' : {
+                'complete' : complete_occs,
+                'error' : error_occs,
+                'running' : running_occs,
+                'waiting' : waiting_occs
             },
             'progress' : progress,
             'projections' : {
@@ -99,32 +99,13 @@ def format_gridset(gridset_id, detail=False):
                 'waiting' : waiting_prjs
             },
             'workflows' : {
-                'complete' : complete_wfs,
-                'error' : error_wfs,
-                'running' : running_wfs,
-                'waiting' : waiting_wfs
+                'complete' : complete_mfs,
+                'error' : error_mfs,
+                'running' : running_mfs,
+                'waiting' : waiting_mfs
             }
         }
     else:
-        gs_mfs = scribe.listMFChains(
-            0, 100, gridsetId=gridset_id, atom=False)
-        
-        mfs_left = 0
-        mfs_running = 0
-        for mf in gs_mfs:
-            if mf.status < JobStatus.GENERAL_ERROR:
-                mfs_left += 1
-            if mf.status == JobStatus.COMPLETE:
-                mfs_running += 1.0
-            elif mf.status > JobStatus.INITIALIZE:
-                # Add .5 to say it is half done
-                mfs_running += 0.5
-        
-        if mfs_left == 0:
-            progress = 1.0
-        else:
-            progress = float(mfs_running) / mfs_left
-
         progress_dict = {
             'progress' : progress
         }
