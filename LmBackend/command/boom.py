ccb6088444fc406d8f17fcd78b753b6eb2e3c73e
CJ Grady <cjgrady@gmail.com>
2020-01-16 21:20:54 -0600

    whitespace clean up

--- LmBackend/command/boom.py
+++ LmBackend/command/boom.py
@@ -7,6 +7,7 @@ from LmBackend.command.base import _LmCommand
 from LmBackend.common.lmconstants import BOOM_SCRIPTS_DIR
 from LmCommon.common.lmconstants import LMFormat
 
+
 # .............................................................................
 class BoomerCommand(_LmCommand):
     """This command will run the boomer
@@ -29,7 +30,8 @@ class BoomerCommand(_LmCommand):
             self.outputs.append(successFile)
             self.opt_args += ' --config_file={}'.format(configFile)
             self.opt_args += ' --success_file={}'.format(successFile)
-    
+
+
 # .............................................................................
 class CatalogBoomCommand(_LmCommand):
     """Catalog boom command
@@ -37,8 +39,8 @@ class CatalogBoomCommand(_LmCommand):
     This command will create makeflows to:
         * catalog boom archive inputs,
         * catalog ScenarioPackage if necessary
-        * create GRIMs, 
-        * create an archive ini file, and 
+        * create GRIMs,
+        * create an archive ini file, and
         * start the Boomer to walk through inputs
     """
     relDir = BOOM_SCRIPTS_DIR
@@ -53,13 +55,13 @@ class CatalogBoomCommand(_LmCommand):
                 for a boom/gridset
         """
         _LmCommand.__init__(self)
-        
+
         if not os.path.exists(config_filename):
             raise Exception(
                 'Missing Boom configuration file {}'.format(config_filename))
         else:
             boomBasename, _ = os.path.splitext(
-                os.path.basename(config_filename)) 
+                os.path.basename(config_filename))
             # Logfile goes to LOG_DIR
             secs = time.time()
             timestamp = "{}".format(
@@ -67,11 +69,11 @@ class CatalogBoomCommand(_LmCommand):
             logname = '{}.{}.{}'.format(
                 self.script_basename, boomBasename, timestamp)
             logfilename = '{}{}'.format(logname, LMFormat.LOG.ext)
-            
+
         # Script args
         self.args = config_filename
         self.args += ' --logname={}'.format(logname)
         if init_makeflow:
             self.args += ' --init_makeflow=True'
-            
+
         self.outputs.append(logfilename)
